25fb8a1c2507b7bd9dea3a446a5a9d745315c0ce
Viktor Kot <vkotyk94@gmail.com>
2023-01-22 17:09:35 +0100

    Task 1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TokenService;
+use Exception;
 use External\Bar\Auth\LoginService;
+use External\Baz\Auth\Authenticator;
+use External\Baz\Auth\Responses\Success;
+use External\Foo\Auth\AuthWS;
+use External\Foo\Exceptions\AuthenticationFailedException;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -24,15 +30,37 @@ class AuthController extends Controller
         $password = $request->input('password');
 
         $barLoginService = new LoginService();
-
-        $res = $barLoginService->login((string) $login, (string) $password);
-
+        $bazLoginService = new Authenticator();
+        $fooLoginService = new AuthWS();
+        $barRes = $barLoginService->login((string) $login, (string) $password);
+        $bazRes = $bazLoginService->auth((string) $login, (string) $password);
         
+        try {
+            $fooRes = $fooLoginService->authenticate((string) $login, (string) $password);
+
+        } catch (AuthenticationFailedException $e) {
+            
+        }
+        if ($barRes || $bazRes instanceof Success || is_null($fooRes)) {
+            return $this->getTokenSuccess();
+        }
 
         return response()->json([
-            'res' => $res,
-            'status' => 'failure',
+            'status' => 'failure'
+        ]);
 
+    }
+
+       
+
+    private function getTokenSuccess()
+    {
+        $tokenService = new TokenService();
+        $token = $tokenService->generate();
+
+        return response()->json([
+            'status' => 'success',
+            'token'  => $token
         ]);
     }
 }
--- app/Services/TokenService.php
+++ app/Services/TokenService.php
@@ -1,30 +1,29 @@
 <?php
 
-namespace App\Utils;
+namespace App\Services;
 
 use Lcobucci\JWT\Encoding\ChainedFormatter;
 use Lcobucci\JWT\Encoding\JoseEncoder;
 use Lcobucci\JWT\Signer\Key\InMemory;
 use Lcobucci\JWT\Signer\Hmac\Sha256;
 use Lcobucci\JWT\Token\Builder;
-use Carbon\Carbon;
 
-class Token 
+class TokenService
 {
 
-    public function generate() 
+    public function generate(): string 
     {
 
         $tokenBuilder = (new Builder(new JoseEncoder(), ChainedFormatter::default()));
         $algorithm    = new Sha256();
         $signingKey   = InMemory::plainText(random_bytes(32));
-        
-        $now   = new DateTimeImmutable]();
+
+        $now   = new \DateTimeImmutable();
         $token = $tokenBuilder
             // Configures the issuer (iss claim)
-            ->issuedBy('http://example.com')
+            ->issuedBy('http://laravel.com')
             // Configures the audience (aud claim)
-            ->permittedFor('http://example.org')
+            ->permittedFor('http://laravel.com')
             // Configures the id (jti claim)
             ->identifiedBy('4f1g23a12aa')
             // Configures the time that the token was issue (iat claim)
@@ -32,14 +31,15 @@ class Token
             // Configures the time that the token can be used (nbf claim)
             ->canOnlyBeUsedAfter($now->modify('+1 minute'))
             // Configures the expiration time of the token (exp claim)
-            ->expiresAt($now->modify('+1 hour'))
+            ->expiresAt($now->modify('+1 minute'))
             // Configures a new claim, called "uid"
             ->withClaim('uid', 1)
             // Configures a new header, called "foo"
             ->withHeader('foo', 'bar')
             // Builds a new token
             ->getToken($algorithm, $signingKey);
-        
-        echo $token->toString();
+
+        return $token->toString();
+
     }
 }
