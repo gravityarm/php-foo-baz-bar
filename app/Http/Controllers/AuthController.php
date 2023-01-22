eaf8c385bfee523682246c0e69e76a6cbe4d2971
Viktor Kot <vkotyk94@gmail.com>
2023-01-22 15:29:08 +0100

    init commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use External\Bar\Auth\LoginService;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+
+class AuthController extends Controller
+{
+    /**
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function login(Request $request): JsonResponse
+    {
+
+        $login = $request->input('login');
+        $password = $request->input('password');
+
+        $barLoginService = new LoginService();
+
+        $res = $barLoginService->login((string) $login, (string) $password);
+
+        
+
+        return response()->json([
+            'res' => $res,
+            'status' => 'failure',
+
+        ]);
+    }
+}
